b4168de2468261c49fac640832f7c5e24017b78c
ZainulislamAdil <34211033+ZainulislamAdil@users.noreply.github.com>
2017-12-03 05:12:35 -0800

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Calculator {
+
+
+	public static void main(String[] args){
+	 Scanner input=new Scanner(System.in);
+	 int number1;
+	 int number2;
+	 int sum;
+	 System.out.print("Enter first number plz Zain:");
+	 number1=input.nextInt();
+	 System.out.print("enter Second number plz Zain");
+	 number2=input.nextInt();
+	 sum=number1+number2;
+	 System.out.print("Sum of numbers is Mr.Zain is this"+sum);
+	}
+	}
